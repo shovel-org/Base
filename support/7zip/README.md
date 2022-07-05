88a7bcd52e54616b82e2d9f5dbb2211d2d715877
Jakub Čábera <cabera.jakub@gmail.com>
2022-07-05 11:07:03 +0200

    typos

--- support/7zip/README.md
+++ support/7zip/README.md
@@ -2,6 +2,6 @@
 
 Zero dependency zip package of 7-zip mainly for NanoServer installations and use for Shovel installer.
 
-All archived were built by [script](https://github.com/shovel-org/Base/main/support/7zip/repack.ps1) using 7zip
+All archives were built by [script](https://github.com/shovel-org/Base/main/support/7zip/repack.ps1) using 7zip.
 
-Original site, source code and artifcats are avaialble at <https://www.7-zip.org>
+Original site, source code and artifacts are available at <https://www.7-zip.org>
--- support/7zip/repack.ps1
+++ support/7zip/repack.ps1
@@ -23,9 +23,9 @@ Set-Content "$PSScriptRoot/README.md" @'
 
 Zero dependency zip package of 7-zip mainly for NanoServer installations and use for Shovel installer.
 
-All archived were built by [script](https://github.com/shovel-org/Base/main/support/7zip/repack.ps1) using 7zip
+All archives were built by [script](https://github.com/shovel-org/Base/main/support/7zip/repack.ps1) using 7zip.
 
-Original site, source code and artifcats are avaialble at <https://www.7-zip.org>
+Original site, source code and artifacts are available at <https://www.7-zip.org>
 '@ -Encoding 'utf8'
 
 $7zPath = (Get-Command -Name '7z' -CommandType 'Application').Source
